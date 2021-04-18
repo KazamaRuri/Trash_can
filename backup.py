e6f621df1571cdebdadf1b3bed46488470f94820
風間瑠璃 <kazamaruri726940@gmail.com>
2021-04-18 11:36:25 +0800

    v1.1

---
+++ backup.py
@@ -0,0 +1,58 @@
+import  tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow import keras
+import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale=1/255)
+validation_datagen = ImageDataGenerator(rescale=1/255)
+
+train_generator = train_datagen.flow_from_directory(
+    'D:/ruri/Projects/Python/Tensorflow/train',
+    target_size=(128,128),
+    batch_size=32
+)
+validation_generator = validation_datagen.flow_from_directory(
+    'D:/ruri/Projects/Python/Tensorflow/validation',
+    target_size=(128,128),
+    batch_size=32
+)
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self,epoch,logs={}):
+        if(logs.get('loss')<0.4):
+            print("\nLoss is low so cancelling training!")
+            self.model.stop_training=True
+callbacks = myCallback()
+
+#fashion_mnist = keras.datasets.fashion_mnist
+#(train_images,train_labels),(test_images,test_labels) = fashion_mnist.load_data()
+#train_images_scaled=train_images/255
+#test_images_scaled=test_images/255
+
+#-----------------------------------------------
+
+model = keras.Sequential()
+model.add(keras.layers.Conv2D(16,(3,3),activation=tf.nn.relu,input_shape=(128,128,3)))
+model.add(keras.layers.MaxPooling2D(2,2))
+model.add(keras.layers.Conv2D(32,(3,3),activation=tf.nn.relu))
+model.add(keras.layers.MaxPooling2D(2,2))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(512,activation=tf.nn.relu))
+model.add(keras.layers.Dense(2,activation=tf.nn.softmax))
+#model.summary()
+
+#model.compile(optimizer=tf.optimizers.Adam(),loss=tf.losses.sparse_categorical_crossentropy,metrics=['accuracy'])
+#model.fit(train_images_scaled.reshape(-1,28,28,1),train_labels,epochs=1,callbacks=[callbacks])
+
+model.compile(optimizer=tf.optimizers.Adam(),loss=tf.losses.categorical_crossentropy,metrics=['accuracy'])
+model.fit(train_generator,epochs=5)
+
+#----------------------------------------
+
+model.evaluate(validation_generator)
+##print(np.argmax(model.predict(test_images_scaled[8].reshape(1,28,28,1))))
+##print(test_labels[8])
+
+#plt.imshow(train_generator[1,:,:,:])
+#plt.show()
---
+++ run.py
@@ -0,0 +1,29 @@
+import  tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow import keras
+import numpy as np
+from PIL import Image
+
+model = keras.models.load_model('Trash_model')
+# model = keras.models.load_model('Trash_model.h5')
+
+img = np.array(Image.open('test.jpg'))
+# plt.imshow(img)
+# plt.show()
+
+sort = np.argmax(model.predict(img.reshape(1,128,128,3)))
+
+print(model.predict(img.reshape(1,128,128,3)))
+
+print(sort)
+if sort==0:
+    print("厨余垃圾")
+elif sort==1:
+    print("废旧电池")
+elif sort==2:
+    print("矿泉水瓶")
+elif sort==3:
+    print("烟头")
+elif sort==4:
+    print("易拉罐")
